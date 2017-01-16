b9de32f650e20867c06563664bdaa075548a5763
Sean Jones <neuralsandwich@gmail.com>
2017-01-16 23:22:57 +0000

    Enable yaml metadata

--- stone.py
+++ stone.py
@@ -1,24 +1,51 @@
 #!/usr/bin/env python3
 
+import collections
 import errno
 import os
 import sys
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2.exceptions import TemplateNotFound
 import json
 import markdown
 
 
-class Page(object):
+class Page(collections.UserDict):
     def __init__(self, site_root, source, target, redirects=None):
-        self.source = os.path.join(site_root, source)
-        self.target = os.path.join(site_root, target)
+        self.data = {}
+        self.source = source
+        self.target = target
+        self.source_path = os.path.join(site_root, source)
+        self.target_path = os.path.join(site_root, target)
+        self.data['content'] = open(self.source_path, "r").read()
+
+    def __contains__(self, key):
+        return str(key) in self.data
+
+    def __missing__(self, key):
+        if str(key) in self.data:
+            return self.data
+        else:
+            raise KeyError(key)
 
     def __repr__(self):
         return "Page(%r, %r)" % (self.source, self.target)
 
     def __str__(self):
-        return open(self.source, "r").read()
+        return self.data['content']
+        return
+    def __setitem__(self, key, item):
+        self.data[str(key)] = item
+
+    def md_to_template_html(self, md_renderer):
+        self.renderer = md_renderer
+        self['content'] = self.renderer.convert(str(self))
+        for key, value in self.renderer.Meta.items():
+            if isinstance(value, list) and len(value) == 1:
+                self.data[key] = value[0]
+            else:
+                self.data[key] = value
 
 
 class SiteConfig(object):
@@ -81,24 +108,35 @@ def main(args):
 
     cfg_loader = ConfigLoader()
     site_configs = cfg_loader.load(site_root)
+    markdown_renderer = markdown.Markdown(
+        extensions=['markdown.extensions.meta'])
     for site in site_configs:
         env = Environment(
             loader=FileSystemLoader(site.templates),
             autoescape=select_autoescape(["html", "xml"]))
 
         for page in site.pages:
-            rendered_html = markdown.markdown(str(page))
+            page.md_to_template_html(markdown_renderer)
+            print("Rendering: %s to %s" % (page.source, page.target))
             try:
-                target_file = open(page.target, "w")
-                target_file.write(env.from_string(rendered_html).render())
-                target_file.close()
+                with open(page.target_path, "w") as target_file:
+                    target_file.write(
+                    env.get_template(page['template']).render(page))
+            except TemplateNotFound as tnf:
+                print(tnf)
+            except KeyError as ke:
+                if str(ke) == '\'template\'':
+                    print('Missing template, rendering markdown only')
+                    env.from_string(page.content).render(page)
+                else:
+                    raise
             except FileNotFoundError as fnf:
                 if fnf.errno != errno.ENOENT:
                     raise
                 else:
                     target_path = os.path.split(page.target)[0]
                     if os.path.isdir(target_path) is False:
-                        os.mkdir(target_path)
+                        os.makedirs(target_path)
 
     return 0
 
