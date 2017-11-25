a593ba5dc14c68a2334077410e4ba2b20f65bcbd
Sean Jones <neuralsandwich@gmail.com>
2017-11-25 12:12:47 +0000

    Update to version 0.1a5

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(name='stone-site',
 
       # Versions should comply with PEP440. For a discussion on single-sourcing
-      version='0.1a4',
+      version='0.1a5',
 
 
       description='Static site generator',
